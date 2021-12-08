e5029c7a054c49cfae3cc3787722335722ef09eb
matheusyanmonteiro <matheusyanmonteiro@gmail.com>
2021-12-08 16:24:25 -0300

    create first basic project

---
+++ CarsCatolog/CarsCatolog/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarsCatolog.Controllers
+{
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "SUV", "Celta", "Sedan" };
+        }
+        public ActionResult <string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
